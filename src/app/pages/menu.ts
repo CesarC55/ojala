b5a5fc31cbabbb2ed66b90fba170c209c068e758
CesarC55 <45542389+CesarC55@users.noreply.github.com>
2018-12-02 13:12:39 -0700

    Add files via upload

---
+++ src/app/pages/menu.ts
@@ -0,0 +1,239 @@
+export let MENU_ITEM = [
+    {
+        path: 'profile',
+        title: 'User Profile',
+        icon: 'user'
+    },
+    {
+        path: 'ui',
+        title: 'Book 1',
+        icon: 'book',
+        children: [
+            {
+                path: 'grid',
+                title: 'Lesson 1'
+            },
+            {
+                path: 'buttons',
+                title: 'Lesson 2'
+            },
+            {
+                path: 'notification',
+                title: 'Lesson 3'
+            },
+            {
+                path: 'tabs',
+                title: 'Lesson 4'
+            },
+            {
+                path: 'file-tree',
+                title: 'Lesson 5'
+            },
+            {
+                path: 'modals',
+                title: 'Lesson 6'
+            },
+            {
+                path: 'progress-bar',
+                title: 'Lesson 7'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 8'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 9'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 10'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 11'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 12'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 13'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 14'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 15'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 16'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 17'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 18'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 19'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 20'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 21'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 22'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 23'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 24'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 25'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 26'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 27'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 28'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 29'
+            },
+            
+            {
+                path: 'progress-bar',
+                title: 'Lesson 30'
+            }
+        ]
+    },
+    {
+        path: 'form',
+        title: 'Book 2',
+        icon: 'book',
+        children: [
+            {
+                path: 'form-inputs',
+                title: 'Lesson 1 y 2'
+            },
+            {
+                path: 'form-layouts',
+                title: 'Lesson 3 y 4'
+            },
+            {
+                path: 'file-upload',
+                title: 'Lesson 5 y 6'
+            },
+            {
+                path: 'ng2-select',
+                title: 'Lesson 7 y 8'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 9 y 10'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 11 y 12'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 13 y 14'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 15 y 16'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 17 y 18'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 19 y 20'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 21 y 22'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 23 y 24'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 25 y 26'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 27 y 28'
+            },
+            {
+                path: 'form-inputs',
+                title: 'Lesson 29 y 30'
+            },
+        ]
+    },
+    {
+        path: 'table',
+        title: 'Book 3',
+        icon: 'book',
+        children: [
+            {
+                path: 'basic-tables',
+                title: 'Opcion 1'
+            },
+            {
+                path: 'data-table',
+                title: 'Opcion 2'
+            }
+        ]
+    },
+];
